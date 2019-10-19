f89310aeb1fc1ef9375a3c51df5396b3b26a00fa
Ashikul Islam Tamal <ashikul.tamal.aust@gmail.com>
2019-10-19 14:12:49 +0600

    add all files. It's all for test.

---
+++ test.js
@@ -0,0 +1,15 @@
+var valueOfPi = 3.1416;
+var yourAge = 1000;
+var a_long_long_long_name = "Some Value";
+
+console.log("Your Age is:", yourAge);
+
+//Multiple variable
+var a = 12;
+var b = 13;
+var c = 14;
+var d = 15;
+
+var a = 12, b = 13, c = 14, d = 15;
+
+console.log(a, b, c, d);
